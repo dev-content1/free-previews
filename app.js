e5b2650ecf68a385719b9a54211ccb8e0f1abf0e
dev-content1 <topadhob@gmail.com>
2026-08-18 15:10:11 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 // =====================================================
 
 const CONFIG = {
-  telegram : "CLEVASDIAL1",
+  telegram : "opendealnow",
   zangi    : "6970835383",
   zangiUrl : "https://services.zangi.com/dl/conversation/6970835383",
 };
